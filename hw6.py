91579b094b09fbf0f4bf66cd77cb69753cb0d662
seoyeseon <127722384+seoyeseon@users.noreply.github.com>
2023-04-19 16:40:09 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1, 10):    #  1부터 9까지 반복
+    for j in range(2, 6):   # 2단부터 5단까지 반복
+        print(j, "x", i, "=", i*j, end = '\t') #tab 만큼 간격을 띄음
+    print () # 줄  바꿈
+print()
+for i in range(1, 10):    #  1부터 9까지 반복
+    for j in range(6, 10):   # 6단부터 9단까지 반복
+        print(j, "x", i, "=", i*j, end = '\t') #tab 만큼 간격을 띄음
+    print () # 줄  바꿈
+
+
