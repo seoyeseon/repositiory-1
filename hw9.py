b761282f49779daf1a49ae86570e84450b63a7ae
seoyeseon <127722384+seoyeseon@users.noreply.github.com>
2023-05-18 01:54:16 +0900

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,27 @@
+class Rectangle:
+    def __init__(self,left-top=0, right-bottom=0):
+        self.__left-top = left-top
+        self.__right-bottom = right-bottom
+
+    def set(self, left-top ,right-bottom):
+        self.__left-top = left-top
+        self.__right-bottom = right-bottom
+
+    def show(self):
+        print(f'({self.__left-top ,self.__right-bottom})')
+
+    def getWidth():
+
+    def getArea():
+    
+    def getHeight():
+
+    def getPerimeter():
+
+
+
+r1 = Rectangle(5, 5, 20, 10)
+a = r1.getArea()
+p = r1.getPerimeter()
+r1.show()
+print(f'\n넓이는 {a}, 둘레는 {p}')
